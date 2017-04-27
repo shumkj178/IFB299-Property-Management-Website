edd85f99b2a48c82cb03f2360dc7ab54f8fa537d
shumkj <seansam06@gmail.com>
2017-04-27 21:54:35 +1000

    New signup forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'static_pages#home'
-  get 'home' => 'static_pages#home'
-  get 'help' => 'static_pages#help'
+  get '/home' => 'static_pages#home'
+  get '/help' => 'static_pages#help'
 
-  get 'signup', to: 'users#new'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   resource 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
