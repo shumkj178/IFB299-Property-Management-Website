aa2a96835ba98fcb822ef669da404e93cd52ab7a
shumkj <seansam06@gmail.com>
2017-05-18 17:33:05 +1000

    fixed bug

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,15 @@ class Property < ApplicationRecord
   def self.search(search)
     where("street LIKE ? OR suburbs LIKE ? OR postcode LIKE ? OR region LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
   end
+
+  validates :house_no, presence: true
+
+  validates :street, presence: true
+
+  validates :suburbs, presence: true
+
+  validates :postcode, presence: true,
+            length: {minimum: 4, maximum: 4}
+
+  validates :region, presence: true
 end
